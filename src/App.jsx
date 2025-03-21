2fe489026f50c423adc8fbd5eb771cd54eb08e52
Lmola23 <molaenriquezl@gmail.com>
2025-03-21 13:09:52 -0400

    Eliminarcion de cocteles

--- src/App.jsx
+++ src/App.jsx
@@ -66,7 +66,6 @@ function App() {
           <EspaguetisSection />
           <BatidosSection />
           <MalteadaSection />
-          <CoteclesSection />
           <BebidasSection />
           <Footer />
         </>
